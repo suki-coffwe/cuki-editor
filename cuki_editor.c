57d493eff5182d5e133933023da59bd0a7c359b9
suki <scarlletecoffwe@gmail.com>
2026-09-06 03:07:26 +0530

    Added fast cursor scroll

--- cuki_editor.c
+++ cuki_editor.c
@@ -28,6 +28,9 @@ int toggleLineNumberShow = 1;		// Start with line numbers showing on.
 #define TAB_LENGTH 4
 
 #define COOL_COLOUR "\x1b[38;5;250m"	//Light grey colour.
+#define FAST_MOVE_STEPS 5
+#define FAST_MOVE_STEPS_SHIFT 10
+#define FAST_MOVE_SHIFT_ADVANCED 25
 
 #define CTRL_KEY(K) ((K) & 0x1f)
 
@@ -38,13 +41,26 @@ enum editorKey
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	CTRL_ARROW_UP = 1100,
+	CTRL_ARROW_DOWN,
+	CTRL_ARROW_LEFT,
+	CTRL_ARROW_RIGHT,
+	SHIFT_ARROW_UP = 1200,
+	SHIFT_ARROW_DOWN,
+	SHIFT_ARROW_LEFT,
+	SHIFT_ARROW_RIGHT,
+	CTRL_SHIFT_ARROW_UP = 1300,
+	CTRL_SHIFT_ARROW_DOWN,
+	CTRL_SHIFT_ARROW_LEFT,
+	CTRL_SHIFT_ARROW_RIGHT,
 	DEL_KEY,
 	HOME_KEY,
 	END_KEY,
 	PAGE_UP,
 	PAGE_DOWN,
 	MOUSE_EVENT,
-	NUM_5
+	NUM_5,
+	F1
 };
 
 // ENUM for UNDO/REDO:
@@ -105,6 +121,9 @@ void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
 char *editorPrompt(char *prompt, void (*callback) (char *, int));
 int editorGetGutterWidth(void);
+void editorInsertNewline(void);
+void editorDelChar(void);
+void editorMoveCursor(int key);
 
 /*** terminal ***/
 
@@ -163,7 +182,7 @@ int editorReadKey()
 
 	if (c == '\x1b')
 	{
-		char seq[3];
+		char seq[6];
 		if (!has_byte() || read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
 		if (!has_byte() || read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
@@ -171,6 +190,46 @@ int editorReadKey()
 		{
 			if (seq[1] == 'P') return DEL_KEY;
 			if (seq[1] == 'E') return NUM_5;
+
+			if (seq[1] == '1')
+			{
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b'; /* ';' */
+				if (read(STDIN_FILENO, &seq[3], 1) != 1) return '\x1b'; /* '5' */
+				if (read(STDIN_FILENO, &seq[4], 1) != 1) return '\x1b'; /* Direction */
+
+				if (seq[2] == ';' && seq[3] == '5')
+				{
+					switch (seq[4])
+					{
+					case 'A': return CTRL_ARROW_UP;
+					case 'B': return CTRL_ARROW_DOWN;
+					case 'C': return CTRL_ARROW_RIGHT;
+					case 'D': return CTRL_ARROW_LEFT;
+					}
+				}
+
+				else if (seq[2] == ';' && seq[3] == '2')
+				{
+					switch (seq[4])
+					{
+					case 'A': return SHIFT_ARROW_UP;
+					case 'B': return SHIFT_ARROW_DOWN;
+					case 'C': return SHIFT_ARROW_RIGHT;
+					case 'D': return SHIFT_ARROW_LEFT;
+					}
+				}
+
+				else if (seq[2] == ';' && seq[3] == '6')
+				{
+					switch (seq[4])
+					{
+					case 'A': return CTRL_SHIFT_ARROW_UP;
+					case 'B': return CTRL_SHIFT_ARROW_DOWN;
+					case 'C': return CTRL_SHIFT_ARROW_RIGHT;
+					case 'D': return CTRL_SHIFT_ARROW_LEFT;
+					}
+				}
+			}
 
 			if (seq[1] >= '0' && seq[1] <= '9')
 			{
@@ -201,19 +260,14 @@ int editorReadKey()
 					case 'F': return END_KEY;
 				}
 			}
-/*
-		if (read(STDIN_FILENO, &seq[0], 1) == 1 && read(STDIN_FILENO, &seq[1], 1) == 1)
+		}
+		else if (seq[0] == 'O' && seq[1] == 'P')
 		{
-			if (seq[0] == '[' && seq[1] == 'P')
-			{
-			return DEL_KEY; // Successfully caught your Delete key
-			}
+			return F1;
 		}
-*/
-		}
+
 		else if (seq[0] == 'O' || seq[0] == 'P')
 		{
-			if (seq[0] == 'P') return DEL_KEY;
 			switch (seq[1])
 			{
 				case 'P': return DEL_KEY;
@@ -521,7 +575,105 @@ void editorInsertChar(int c)
 	E.cx++;
 }
 
+void editorInsertString(const char *s)
+{
+	if (!s) return;
+	
+	while (*s)
+	{
+		if (*s == '\n')	// New Line
+		{
+			editorInsertNewline();
+			s++;
+		}
+		else if (*s == '\r')	// Whatever it is
+		{
+			editorInsertChar('\r');
+			s++;
+		}
+/*
+		else if (*s == '\t')	// Tab
+		{
+			editorInsertChar('\t');
+		}
+*/
+		else if (*s == '\b')	// Backspace
+		{
+			editorDelChar();
+			s++;
+		}
+		else if (strncmp(s, "\x1b[A", 3) == 0)	// Cursor Up
+		{
+			editorMoveCursor(ARROW_UP);
+			s += 3;
+		}
+		else if (strncmp(s, "\x1b[B", 3) == 0)	// Cursor Down
+		{
+			editorMoveCursor(ARROW_DOWN);
+			s += 3;
+		}
+		else if (strncmp(s, "\x1b[C", 3) == 0)	// Cursor Right
+		{
+			editorMoveCursor(ARROW_RIGHT);
+			s += 3;
+		}
+		else if (strncmp(s, "\x1b[D", 3) == 0)	// Cursor Left
+		{
+			editorMoveCursor(ARROW_LEFT);
+			s += 3;
+		}
+/*
+		else if (strncmp(s, "\x1b[F", 3) == 0)	// Cursor End
+		{
+			if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
+			s += 3;
+		}
+*/
+		else if (strncmp(s, "\x1b[F", 3) == 0) {
+			if (E.numrows > 0 && E.cy < E.numrows) {
+				E.cx = E.row[E.cy].size;
+			}
+			else
+			{
+				E.cx = 0;
+			}
+			s += 3;
+		}
+
+		else if (strncmp(s, "\x1b[H", 3) == 0)
+		{
+			E.cx = 0;
+			s += 3;
+		}
+		else
+		{
+			editorInsertChar(*s);
+			s++;
+		}
+	}
+}
+
 void editorInsertNewline(void)
+{
+	if (E.cx == 0)
+	{
+		editorInsertRow(E.cy, "", 0);
+	}
+
+	else
+	{
+		erow *row = &E.row[E.cy];
+		editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+		row = &E.row[E.cy];
+		row->size = E.cx;
+		row->chars[row->size] = '\0';
+		editorUpdateRow(row);
+	}
+	E.cy++;
+	E.cx = 0;
+}
+
+void editorAppendedInsertNewline(void)
 {
 	if (E.cx == 0)
 	{
@@ -551,10 +703,10 @@ void editorInsertNewline(void)
 			erow *new_row = &E.row[E.cy + 1];
 			new_row->chars = realloc(new_row->chars, new_row->size + indent_len + 1);
 			
-			/* Shift existing contents right */
+			// Shift existing contents right
 			memmove(&new_row->chars[indent_len], new_row->chars, new_row->size + 1);
 			
-			/* Copy the indentation chars */
+			//Copy the indentation chars
 			memcpy(new_row->chars, E.row[E.cy].chars, indent_len);
 			new_row->size += indent_len;
 			editorUpdateRow(new_row);
@@ -570,6 +722,7 @@ void editorInsertNewline(void)
 	E.cy++;
 	E.cx = 0;
 }
+
 
 void editorDelChar()
 {
@@ -1176,17 +1329,12 @@ void editorProcessKeypress()
 			break;
 
 		case NUM_5:
-		{
-				char *s = "printf(\"\");";
-				while (*s)
-				{
-					editorInsertChar(*s++);
-				}
-				editorMoveCursor(ARROW_LEFT);
-				editorMoveCursor(ARROW_LEFT);
-				editorMoveCursor(ARROW_LEFT);
-				break;
-		}
+			editorInsertString("#include <stdio.h>\n\nint main()\n{\n\n\n\treturn 0;\n\b}\x1b[A\x1b[A\x1b[A\t");
+			break;
+
+		case F1:
+			editorInsertString("\x1b[F\n{\n\t\n\b}\x1b[A");
+			break;
 
 		case CTRL_KEY('z'):
 			editorUndo();
@@ -1290,6 +1438,56 @@ void editorProcessKeypress()
 		case ARROW_LEFT:
 		case ARROW_RIGHT:
 			editorMoveCursor(c);
+			break;
+
+		case CTRL_ARROW_UP:
+			for (int i = 0; i < FAST_MOVE_STEPS; i++) editorMoveCursor(ARROW_UP);
+			break;
+
+		case CTRL_ARROW_DOWN:
+			for (int i = 0; i < FAST_MOVE_STEPS; i++) editorMoveCursor(ARROW_DOWN);
+			break;
+
+		case CTRL_ARROW_LEFT:
+			for (int i = 0; i < FAST_MOVE_STEPS; i++) editorMoveCursor(ARROW_LEFT);
+			break;
+
+		case CTRL_ARROW_RIGHT:
+			for (int i = 0; i < FAST_MOVE_STEPS; i++) editorMoveCursor(ARROW_RIGHT);
+			break;
+
+
+		case SHIFT_ARROW_UP:
+			for (int i = 0; i < FAST_MOVE_STEPS_SHIFT; i++) editorMoveCursor(ARROW_UP);
+			break;
+
+		case SHIFT_ARROW_DOWN:
+			for (int i = 0; i < FAST_MOVE_STEPS_SHIFT; i++) editorMoveCursor(ARROW_DOWN);
+			break;
+
+		case SHIFT_ARROW_LEFT:
+			for (int i = 0; i < FAST_MOVE_STEPS_SHIFT; i++) editorMoveCursor(ARROW_LEFT);
+			break;
+
+		case SHIFT_ARROW_RIGHT:
+			for (int i = 0; i < FAST_MOVE_STEPS_SHIFT; i++) editorMoveCursor(ARROW_RIGHT);
+			break;
+
+
+		case CTRL_SHIFT_ARROW_UP:
+			for (int i = 0; i < FAST_MOVE_SHIFT_ADVANCED; i++) editorMoveCursor(ARROW_UP);
+			break;
+
+		case CTRL_SHIFT_ARROW_DOWN:
+			for (int i = 0; i < FAST_MOVE_SHIFT_ADVANCED; i++) editorMoveCursor(ARROW_DOWN);
+			break;
+
+		case CTRL_SHIFT_ARROW_LEFT:
+			for (int i = 0; i < FAST_MOVE_SHIFT_ADVANCED; i++) editorMoveCursor(ARROW_LEFT);
+			break;
+
+		case CTRL_SHIFT_ARROW_RIGHT:
+			for (int i = 0; i < FAST_MOVE_SHIFT_ADVANCED; i++) editorMoveCursor(ARROW_RIGHT);
 			break;
 
 		case CTRL_KEY('l'):
